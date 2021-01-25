df636a19e59e32a0b1ba4fab7b32de8ed569e496
Paunon, Koleen B <koleen.b.paunon@accenture.com>
2021-01-26 00:41:01 +0800

    adding rejection handler

--- route.js
+++ route.js
@@ -5,33 +5,38 @@ const chromeLauncher = require("chrome-launcher")
 
 // As soon as the client hit the /scan/ endpoint, the function below will run
 // Sending back the JSON payload of Lighthouse audit results for "Audit" category
-router.post("/", async (req, res) => {
-  // Programatically running the Lighthouse audit process on the backend
-  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] })
-  const options = {
-    logLevel: "info",
-    output: "json",
-    port: chrome.port,
+router.post("/", async (req, res, next) => {
+  try {
+    // Programatically running the Lighthouse audit process on the backend
+    const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] })
+    const options = {
+      logLevel: "info",
+      output: "json",
+      port: chrome.port,
+    }
+    // The `req.body.url` will be the URL to be audited and comes from the client as parameter.
+    // Example: "https://web.dev"
+    const runnerResult = await lighthouse(req.body.url, options)
+
+    // Sample logging for the Performance Report
+    console.log("Report is done for", runnerResult.lhr.finalUrl)
+    console.log(
+      "Performance score was",
+      runnerResult.lhr.categories.performance.score * 100
+    )
+
+    // Killing the Chrome launcher on the server as soon as the audit completes
+    await chrome.kill()
+
+    let networkRequests = runnerResult.lhr.audits
+    // Send back the `networkRequests` object to the client
+    // This contains the needed JSON data for the greenhouse computation
+    // This returned JSON should be stored on the app's state
+    res.send(networkRequests)
+  } catch (err) {
+    next(err)
+    console.error("APPLICATION ERROR: ", err)
   }
-  // The `req.body.url` will be the URL to be audited and comes from the client as parameter.
-  // Example: "https://web.dev"
-  const runnerResult = await lighthouse(req.body.url, options)
-
-  // Sample logging for the Performance Report
-  console.log("Report is done for", runnerResult.lhr.finalUrl)
-  console.log(
-    "Performance score was",
-    runnerResult.lhr.categories.performance.score * 100
-  )
-
-  // Killing the Chrome launcher on the server as soon as the audit completes
-  await chrome.kill()
-
-  let networkRequests = runnerResult.lhr.audits
-  // Send back the `networkRequests` object to the client
-  // This contains the needed JSON data for the greenhouse computation
-  // This returned JSON should be stored on the app's state
-  res.send(networkRequests)
 })
 
 module.exports = router
